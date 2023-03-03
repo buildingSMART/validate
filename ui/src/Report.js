5b7bec71bc408a84ca9a7e235aa6346ee794806d
johltn <luttun.j@gmail.com>
2023-03-02 21:44:35 -0500

    Add side menu to the report

--- ui/src/Report.js
+++ ui/src/Report.js
@@ -9,12 +9,13 @@ import SyntaxResult from './SyntaxResult.js'
 import SchemaResult from './SchemaResult';
 import BsddTreeView from './BsddTreeView'
 import GherkinResults from './GherkinResult';
+import SideMenu from './SideMenu';
 
 import { useEffect, useState, useContext } from 'react';
 import { FETCH_PATH } from './environment'
 import { PageContext } from './Page';
 
-function Report({kind}) {
+function Report({ kind }) {
   const context = useContext(PageContext);
 
   const [isLoggedIn, setLogin] = useState(false);
@@ -28,7 +29,7 @@ function Report({kind}) {
   const [commitId, setCommitId] = useState("")
 
   useEffect(() => {
-    fetch(context.sandboxId?`${FETCH_PATH}/api/sandbox/me/${context.sandboxId}`:`${FETCH_PATH}/api/me`)
+    fetch(context.sandboxId ? `${FETCH_PATH}/api/sandbox/me/${context.sandboxId}` : `${FETCH_PATH}/api/me`)
       .then(response => response.json())
       .then((data) => {
         if (data["redirect"] !== undefined) {
@@ -61,59 +62,96 @@ function Report({kind}) {
     console.log("Report data ", reportData)
     return (
       <div>
-        <Grid
+        <Grid direction="column"
           container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="space-between"
-          style={{ minHeight: '100vh', gap: '15px', backgroundColor: 'rgb(238, 238, 238)', border: context.sandboxId?'solid 12px red':'none'}}
-        >
-          <ResponsiveAppBar user={user} />
-          {context.sandboxId && <h2
           style={{
-            background: "red",
-            color: "white",
-            marginTop: "-16px",
-            lineHeight: "30px",
-            padding: "12px",
-            borderRadius: "0 0 16px 16px"
-          }}
-           >Sandbox for <b>{prTitle}</b></h2>}
-          <Disclaimer />
+            minHeight: '100vh', alignItems: 'stretch',
+          }} >
+          <ResponsiveAppBar user={user} />
+          <Grid
+            container
+            flex={1}
+            direction="row"
+            style={{
+            }}
+          >
+            <SideMenu />
+            
+            <Grid
+              container
+              flex={1}
+              direction="column"
+              style={{
+                justifyContent: "space-between",
+                overflow: 'scroll',
+                boxSizing: 'border-box',
+                maxHeight: '90vh',
+                overflowX: 'hidden'
+              }}
+            >
+              <div style={{
+                gap: '10px',
+                flex: 1
+              }}>
+                <Grid
+                  container
+                  spacing={0}
+                  direction="column"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  style={{
+                    minHeight: '100vh', gap: '15px', backgroundColor: 'rgb(242 246 248)',
+                    border: context.sandboxId ? 'solid 12px red' : 'none'
+                  }}
+                >
+                  {context.sandboxId && <h2
+                    style={{
+                      background: "red",
+                      color: "white",
+                      marginTop: "-16px",
+                      lineHeight: "30px",
+                      padding: "12px",
+                      borderRadius: "0 0 16px 16px"
+                    }}
+                  >Sandbox for <b>{prTitle}</b></h2>}
+                  <Disclaimer />
 
-          <h2>Validation Report</h2>
+                  <h2>Validation Report</h2>
 
-          <GeneralTable data={reportData} type={"general"} />
-          <GeneralTable data={reportData} type={"overview"} />
+                  <GeneralTable data={reportData} type={"general"} />
+                  <GeneralTable data={reportData} type={"overview"} />
 
-          {
-            (kind === "syntax_and_schema")
-              ? <SyntaxResult status={reportData["model"]["status_syntax"]} summary={"Syntax"} content={reportData["results"]["syntax_result"]} />
-              : null
-          }
-          {
-            (kind === "syntax_and_schema")
-              ? <SchemaResult status={reportData["model"]["status_schema"]} summary={"Schema"} content={reportData["results"]["schema_result"]} instances={reportData.instances} />
-              : null
-          }
-          {
-            (kind === "bsdd")
-              ? <BsddTreeView status={reportData["model"]["status_bsdd"]} summary={"bSDD"} bsddResults={reportData["results"]["bsdd_results"]} />
-              : null
-          }
-          {
-            (kind === "rules")
-              ? <GherkinResults status={reportData["model"]["status_ia"]} gherkin_task={reportData.tasks["implementer_agreements_task"]} task_type="implementer_agreements_task" />
-              : null
-          }
-          {
-            (kind === "rules")
-              ? <GherkinResults status={reportData["model"]["status_ip"]} gherkin_task={reportData.tasks["informal_propositions_task"]} task_type="informal_propositions_task" />
-              : null
-          }
-          
-          <Footer />
+                  {
+                    (kind === "syntax_and_schema")
+                      ? <SyntaxResult status={reportData["model"]["status_syntax"]} summary={"Syntax"} content={reportData["results"]["syntax_result"]} />
+                      : null
+                  }
+                  {
+                    (kind === "syntax_and_schema")
+                      ? <SchemaResult status={reportData["model"]["status_schema"]} summary={"Schema"} content={reportData["results"]["schema_result"]} instances={reportData.instances} />
+                      : null
+                  }
+                  {
+                    (kind === "bsdd")
+                      ? <BsddTreeView status={reportData["model"]["status_bsdd"]} summary={"bSDD"} bsddResults={reportData["results"]["bsdd_results"]} />
+                      : null
+                  }
+                  {
+                    (kind === "rules")
+                      ? <GherkinResults status={reportData["model"]["status_ia"]} gherkin_task={reportData.tasks["implementer_agreements_task"]} task_type="implementer_agreements_task" />
+                      : null
+                  }
+                  {
+                    (kind === "rules")
+                      ? <GherkinResults status={reportData["model"]["status_ip"]} gherkin_task={reportData.tasks["informal_propositions_task"]} task_type="informal_propositions_task" />
+                      : null
+                  }
+
+                  <Footer />
+                </Grid>
+              </div>
+            </Grid>
+          </Grid>
         </Grid>
       </div>
     );
