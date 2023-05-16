388e668a8efc4288f55bf7ef08a50cf4e63e3444
Thomas Krijnen <t.krijnen@gmail.com>
2023-05-16 09:14:41 +0000

    Filter duplicate rule passed messages

--- application/checks/check_gherkin.py
+++ application/checks/check_gherkin.py
@@ -1,4 +1,5 @@
 import os, sys
+import functools
 
 if os.environ.get('GHERKIN_REPO_DIR'):
     sys.path.insert(0, os.environ.get('GHERKIN_REPO_DIR'))
@@ -13,9 +14,10 @@ def perform(ifc_fn, task_id, rule_type=gherkin_rules.RuleType.ALL):
         file_id = model.id
 
         results = list(gherkin_rules.run(ifc_fn, instance_as_str=False, rule_type=rule_type))
-        instances = set((inst_id, inst_type) for _a, _b, _c, (inst_id, inst_type), _d in filter(lambda r: r[3], results))
 
+        @functools.lru_cache(maxsize=10240)
         def commit_instance(p):
+            if p is None: return None
             inst_id, inst_type = p
             instance = database.ifc_instance(f"#{inst_id}", inst_type, file_id)
             session.add(instance)
@@ -24,11 +26,16 @@ def perform(ifc_fn, task_id, rule_type=gherkin_rules.RuleType.ALL):
             session.commit()
             return instance_id
 
-        instance_ids = map(commit_instance, instances)
-        instance_to_id = dict(zip(instances, instance_ids))
+        seen = set()
 
         for feature_name, feature_url, step, inst, message in results:
-            session.add(database.gherkin_result(task_id, feature_name, feature_url, step, instance_to_id.get(inst), message))
+            if message == 'Rule passed':
+                key = (feature_name, feature_url, step)
+                if key in seen:
+                    continue
+                seen.add(key)
+
+            session.add(database.gherkin_result(task_id, feature_name, feature_url, step, commit_instance(inst), message))
 
         if gherkin_rules.RuleType.INFORMAL_PROPOSITION in rule_type:
             model.status_ip = 'i' if [r for r in results if r[4] not in ('Rule passed', 'Rule disabled')] else 'v'
