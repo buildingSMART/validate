df87fb94ed9ab151312159dd599c1779055bbf8a
Thomas Krijnen <t.krijnen@gmail.com>
2023-05-31 07:50:16 +0000

    Bump version

--- ui/src/SideMenu.js
+++ ui/src/SideMenu.js
@@ -48,7 +48,7 @@ export default function SideMenu() {
                 <Divider />
                     <ListItem key={"test"} disablePadding>
                         <ListItemButton>
-                            <ListItemText style={{ textAlign: 'center' }} primary={`${context["environment"]} v0.5.2`} />
+                            <ListItemText style={{ textAlign: 'center' }} primary={`${context["environment"]} v0.5.3`} />
                         </ListItemButton>
                     </ListItem>
                 </List>
