a504114bd16f8bbd273a80b112cc2ecb282bdcd3
Geert Hesselink <54070862+Ghesselink@users.noreply.github.com>
2024-07-25 14:30:27 +0200

    Update backend/core/settings.py
    Co-authored-by: Thomas Krijnen <t.krijnen@gmail.com>

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -361,7 +361,8 @@ LOGIN_CALLBACK_URL = os.environ.get("CALLBACK_URL", f"{PUBLIC_URL}/callback")
 POST_LOGIN_REDIRECT_URL = os.environ.get("POST_LOGIN_REDIRECT_URL", f"{PUBLIC_URL}/dashboard")
 
 # whitelisting of users
-USE_WHITELIST = ast.literal_eval(os.environ.get("USE_WHITELIST", False))
+USE_WHITELIST = ast.literal_eval(os.environ.get("USE_WHITELIST", 'False'))
+
 
 AUTHLIB_OAUTH_CLIENTS = {
     'b2c': {
