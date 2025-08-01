d1b3c688eeeed72d67c8c980d75c257c4b1ccaae
rw-bsi <raphael+buildingsmart@xeneo.biz>
2025-08-01 22:24:25 +0100

    made trailing slash optional + added e2e tests

--- e2e/tests/validate_api.test.js
+++ e2e/tests/validate_api.test.js
@@ -30,6 +30,29 @@ test.describe('API - ValidationRequest', () => {
         expect(response.status()).toBe(201);
     });
 
+    test('POST without a trailing slash accepts a valid file', async ({ request }) => {
+
+        // try to post a valid file
+        const file_path = 'e2e/fixtures/valid_file.ifc';
+        const file_name = basename(file_path);
+        const file = new File([readFileSync(file_path)], file_name);
+        const form = new FormData();
+        form.append('file', file);
+        form.append('file_name', file_name);
+
+        let hash = btoa(TEST_CREDENTIALS);
+        const response = await request.post(`${BASE_URL}/api/validationrequest`, {
+            headers: {
+                'Authorization': `Basic ${hash}`,
+            },
+            multipart: form
+        });
+
+        // check if the response is correct - 201 Created
+        expect(response.statusText()).toBe('Created');
+        expect(response.status()).toBe(201);
+    });
+
     test('POST implements file size limit', async ({ request }) => {
 
         // try to post a large file
@@ -52,6 +75,76 @@ test.describe('API - ValidationRequest', () => {
         expect(response.statusText()).toBe('Request Entity Too Large');
         expect(response.status()).toBe(413); 
         expect(await response.json()).toEqual({ message: 'File size exceeds allowed file size limit (256 MB).' });
+    });
+
+    test('GET returns a list', async ({ request }) => {
+
+        // try to post a valid file
+        const file_path = 'e2e/fixtures/valid_file.ifc';
+        const file_name = basename(file_path);
+        const file = new File([readFileSync(file_path)], file_name);
+        const form = new FormData();
+        form.append('file', file);
+        form.append('file_name', file_name);
+
+        let hash = btoa(TEST_CREDENTIALS);
+        let response = await request.post(`${BASE_URL}/api/validationrequest/`, {
+            headers: {
+                'Authorization': `Basic ${hash}`,
+            },
+            multipart: form
+        });
+
+        // retrieve list of ValidationRequests
+        response = await request.get(`${BASE_URL}/api/validationrequest`, {
+            headers: {
+                'Authorization': `Basic ${hash}`,
+            }
+        });
+
+        // check if the response is correct - 200 OK
+        expect(response.statusText()).toBe('OK');
+        expect(response.status()).toBe(200);
+        const data = await response.json();
+        expect(data).toBeInstanceOf(Array);
+        expect(data.length).toBeGreaterThan(0);
+        expect(data[0]).toHaveProperty('public_id');
+        expect(data[0]).toHaveProperty('file_name');
+    });
+
+    test('GET without trailing slash returns a list', async ({ request }) => {
+
+        // try to post a valid file
+        const file_path = 'e2e/fixtures/valid_file.ifc';
+        const file_name = basename(file_path);
+        const file = new File([readFileSync(file_path)], file_name);
+        const form = new FormData();
+        form.append('file', file);
+        form.append('file_name', file_name);
+
+        let hash = btoa(TEST_CREDENTIALS);
+        let response = await request.post(`${BASE_URL}/api/validationrequest`, {
+            headers: {
+                'Authorization': `Basic ${hash}`,
+            },
+            multipart: form
+        });
+
+        // retrieve list of ValidationRequests
+        response = await request.get(`${BASE_URL}/api/validationrequest`, {
+            headers: {
+                'Authorization': `Basic ${hash}`,
+            }
+        });
+
+        // check if the response is correct - 200 OK
+        expect(response.statusText()).toBe('OK');
+        expect(response.status()).toBe(200);
+        const data = await response.json();
+        expect(data).toBeInstanceOf(Array);
+        expect(data.length).toBeGreaterThan(0);
+        expect(data[0]).toHaveProperty('public_id');
+        expect(data[0]).toHaveProperty('file_name');
     });
 
 });
