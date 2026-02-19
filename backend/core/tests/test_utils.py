4b04760587036810ba176ce7df189d1d4da5e535
rw-bsi <raphael+buildingsmart@xeneo.biz>
2026-02-19 20:03:04 +0000

    even more tests

--- backend/core/tests/test_utils.py
+++ backend/core/tests/test_utils.py
@@ -25,15 +25,8 @@ class TestDeterministicAltNameStorage(TransactionTestCase):
             assert '.txt' in name
             assert len(name.split('test_')[1].split('.txt')[0]) == 7
 
-    def test_generates_alternative_name_path_traversal(self):
-
-        with tempfile.TemporaryDirectory() as tmpdir:
-            storage = DeterministicAltNameStorage(location=tmpdir)
-            with self.assertRaises(SuspiciousFileOperation):
-                storage.get_available_name('../folder/test.txt')
-
     def test_generates_unique_name_on_collision(self):
-        
+
         with tempfile.TemporaryDirectory() as tmpdir:
             storage = DeterministicAltNameStorage(location=tmpdir)
             name1 = storage.get_available_name('test.txt')
@@ -44,3 +37,38 @@ class TestDeterministicAltNameStorage(TransactionTestCase):
             print('Generated name2: ', name2)
             assert name1 != name2
             assert len(name2.split('test_')[1].split('.txt')[0]) == 7
+        
+    def test_generates_alternative_name_path_traversal(self):
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            storage = DeterministicAltNameStorage(location=tmpdir)
+            with self.assertRaises(SuspiciousFileOperation):
+                storage.get_available_name('../folder/test.txt')
+
+    def test_generates_alternative_name_empty_name(self):
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            storage = DeterministicAltNameStorage(location=tmpdir)
+            with self.assertRaises(SuspiciousFileOperation):
+                storage.get_available_name('')
+
+    def test_generates_alternative_name_single_dot(self):
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            storage = DeterministicAltNameStorage(location=tmpdir)
+            with self.assertRaises(SuspiciousFileOperation):
+                storage.get_available_name('.')
+
+    def test_generates_alternative_name_double_dot(self):
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            storage = DeterministicAltNameStorage(location=tmpdir)
+            with self.assertRaises(SuspiciousFileOperation):
+                storage.get_available_name('..')
+
+    def test_generates_alternative_slash(self):
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            storage = DeterministicAltNameStorage(location=tmpdir)
+            with self.assertRaises(SuspiciousFileOperation):
+                storage.get_available_name('/')
